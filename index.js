83683a99488f5855cad9771c05625747c0e863ef
cynthia <cchelangat9892gmail.com>
2022-09-12 14:01:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
 /* Your Code Here */
+function createEmployeeRecord(arr) {
+    return {
+        firstName : arr[0],
+        familyName : arr[1],
+        title : arr[2],
+        payPerHour : arr[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+}
+    function createEmployeeRecords(employeeData) {
+       return employeeData.map(function(arr) {
+        return createEmployeeRecord(arr)
+       })
+    }
+    function createTimeInEvent(dateStamp) {
+        let [date, hour] = dateStamp.split(" ")
+        this.timeInEvents.push({
+            type : "TimeIn",
+            hour: parseInt(hour, 10),
+            date,
+        })
+        return this
+    }
+    function createTimeOutEvent(dateStamp) {
+        let [date, hour] = dateStamp.split(" ")
+        this.timeOutEvents.push({
+            type: "TimeOut",
+            hour: parseInt(hour, 10),
+            date,
+        })
+        return this 
+    }
+    function hoursWorkedOnDate(date) {
+        let timeIn = this.timeInEvents.find(function(e) {
+            return e.date === date
+        })
+        let timeOut = this.timeOutEvents.find(function(e) {
+            return e.date === date
+        })
+        return (timeOut.hour - timeIn.hour) / 100
+    }
+    function wagesEarnedOnDate(date) {
+        let wage = hoursWorkedOnDate.call(this,date) *
+        this.payPerHour
+        return parseFloat(wage.toString())
+    }
+    function findEmployeeByFirstName(srcArray, firstName) {
+        return srcArray.find(function(record) {
+            return record.firstName === firstName
+        })
+    }
+    function calculatePayroll(array) {
+        return array.reduce(function(data, record) {
+            return data + allWagesFor.call(record)
+        }, 0)
+    }
+
 
 
 /*
